777987f820f4d87cb3805c77d246a02b3bc1fe6b
psarge2 <paul@psarge.dev>
2022-10-07 17:50:02 -0400

    created new, edit, and create actions.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,28 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(articles_params)
+    if @article.save
+      redirect_to articles_path
+    else
+      render :new
+   end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  private
+  def articles_params
+    params.require(:article).permit(:title, :content)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "/articles/:id/edit", to: "articles#edit"
+  post "/articles", to: "articles#create"
+  get "/articles/new", to: "articles#new"
   get "/articles", to: "articles#index"
+  
   get "/articles/:id", to: "articles#show", as: :article
   # Defines the root path route ("/")
   root "articles#index"
