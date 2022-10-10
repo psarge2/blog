c0fd269a519aaa7ba9780a177163ba2da500c129
psarge2 <paul@psarge.dev>
2022-10-10 17:06:52 -0400

    Added comment model and controller for comments section on the show view. Also added comments and article id to db and added validations for comment body on arcticle show view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   root "articles#index"
-  resources :articles 
+  resources :articles do
+    resources :comments
+  end
   #delete "/articles/:id", to: "articles#destroy"
   #patch "/articles/:id", to: "articles#update"
   #put "articles/:id", to: "articles#update"
